6b49c533c4bfcb87da8bf74d3805c9c20eb5b073
rhanakeawe <rhanakeawe@gmail.com>
2025-08-25 22:02:10 -0700

    new lock
    locked

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 require("config.lazy")
+
+vim.cmd([[
+" use <tab> to trigger completion and navigate to the next complete item
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+inoremap <silent><expr> <Tab>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
+]])
